78e64cba6878868a3b5253592ed35dafb2bf7316
omaralkhuly18 <omaralkhuly18.com>
2024-07-28 17:03:33 -0700

    commitL three

--- js/javascript.js
+++ js/javascript.js
@@ -164,3 +164,9 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const marquee = document.querySelector('.marquee');
+    const clone = marquee.cloneNode(true);
+    marquee.parentElement.appendChild(clone);
+});
